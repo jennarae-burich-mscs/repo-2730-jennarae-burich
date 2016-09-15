f6a013ff8d360eb9478c37de77f436665afb5de1
Jenna <Jenna@DESKTOP-SDMD0PI>
2016-09-14 22:23:40 -0500

    Added 1G

---
+++ WindowsApplication1/Main Form.vb
@@ -0,0 +1,23 @@
+﻿' Name:         Average Test Score Calculator
+' Purpose:      Calculates average test score
+' Programmer:   JennaRae Burich on 9/14/2016
+
+Option Strict On
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtScore1.Text = String.Empty
+        txtScore2.Text = String.Empty
+        txtScore3.Text = String.Empty
+        lblAvgScore.Text = String.Empty
+        txtScore1.Focus()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        lblAvgScore.Text = Format((Val(txtScore1.Text) + Val(txtScore2.Text) + Val(txtScore3.Text)) / 3, "standard")
+    End Sub
+End Class
